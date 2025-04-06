9c57d288596ad521e357550c6bed1c900b5593d6
bRight36691 <141995847+bRight36691@users.noreply.github.com>
2025-04-06 17:55:45 +0700

    add app and app.test

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
-const dayOfTheWeek = (date = new Date()) => {
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-    return days[date.getDay()];
-}
+const { dayOfTheWeek } = require('./app');
+const { monthOfTheYear } = require('./app');
 
 
-try {
-    document.getElementById('day').innerText = dayOfTheWeek();
-} catch(err) {}
-
-
-exports.dayOfTheWeek = dayOfTheWeek;
+test('getDay returns the long-format day of the week', () => {
+    const day = dayOfTheWeek( new Date('3/11/2020') );
+    expect( day ).toBe('Wednesday');
+});
+test('getDay returns the month of the year', () => {
+    const month = monthOfTheYear( new Date('3/11/2020') );
+    expect( month ).toBe('November');
+});
